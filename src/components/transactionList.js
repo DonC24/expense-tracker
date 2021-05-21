ffae3635755243903dda348f8854cf4e37a2a35d
DonC24 <51435215+DonC24@users.noreply.github.com>
2021-05-21 14:55:17 +0800

    Added context to Transcation List

--- src/components/transactionList.js
+++ src/components/transactionList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext, GlobalProvider } from '../context/GlobalState';
 
 export const TransactionList = () => {
+    const context = useContext(GlobalContext);
+
+    console.log(context);
+
     return (
         <React.Fragment>
             <h3>Transaction History</h3>

ff18f225d1d8e77d97c02016f606c67f3307517f
DonC24 <51435215+DonC24@users.noreply.github.com>
2021-05-21 17:20:19 +0800

    Added total income or expense. Added delect transaction and total balance.

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,11 +14,23 @@ const initialState = {
 //Create Context
 export const GlobalContext = createContext(initialState);
 
+//Actions
+function deleteTransaction(id) {
+    dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+    });
+}
+
 //Provider Component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (<GlobalContext.Provider value={{transactions: state.transactions}}>
+    return (<GlobalContext.Provider 
+    value={{
+        transactions: state.transactions,
+        deleteTransaction
+        }}>
         {children}
     </GlobalContext.Provider>);
 }
